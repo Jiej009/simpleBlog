1ea43bdc1c2592c2a779a9f53e312576ce4f5216
tiankang <kangtian@6tiantian.com>
2020-06-29 17:49:04 +0800

    sql

---
+++ src/main/resources/sql/mysql.sql
@@ -0,0 +1,34 @@
+-- 创建 博客表 --
+create table blog (
+    id int(10) unsigned not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    status tinyint(4) not null,
+    create_time timestamp not null default current_timestamp,
+    update_time timestamp not null default current_timestamp,
+    PRIMARY key (id)
+) ENGINE=InnoDB AUTO_INCREMENT=167 DEFAULT CHARSET=utf8mb4 COMMENT '博客表';
+
+insert into blog (id, name, status) value (1, '第一篇文章', 0);
+
+-- 博客内容表 --
+create table blog_comment (
+    id int(10) unsigned not null AUTO_INCREMENT,
+    blog_id int(10) not null,
+    content MEDIUMTEXT null,
+    create_time timestamp not null default current_timestamp,
+    update_time timestamp not null default current_timestamp,
+    primary key (id)
+) ENGINE=InnoDB AUTO_INCREMENT=167 DEFAULT CHARSET=utf8mb4 COMMENT '博客表内容';
+
+insert into blog_comment (id, blog_id, content) VALUES (1, 1, '第一篇文章的内容');
+
+create table comment (
+    id int(10) unsigned not null AUTO_INCREMENT,
+    blog_id int(10) not null,
+    comment varchar(1024) not null,
+    create_time timestamp not null default current_timestamp,
+    update_time timestamp not null default current_timestamp,
+    primary key (id)
+) ENGINE=InnoDB AUTO_INCREMENT=167 DEFAULT CHARSET=utf8mb4 COMMENT '评论';
+
+insert into comment (id, blog_id, comment) VALUES (1, 1, '一个评论');
